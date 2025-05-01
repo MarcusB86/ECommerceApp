fa0edb88a8d74d8e903e583a64a1e8965499ed33
MarcusB86 <marcusb08@gmail.com>
2025-04-30 20:25:31 -0400

    basic Navbar complete with unresponsive links to Home, Cart, Loginpage and  minimal css styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">💳 Name Pending..</Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/">Home 🏚️</Link>
+        <Link to="/cart">Cart 🛒</Link>
+        <Link to="/login">Login 🔓</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
