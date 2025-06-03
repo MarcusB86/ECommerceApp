f56175ea4e155dc0255a7e7594219654149839ae
MarcusB86 <marcusb08@gmail.com>
2025-06-03 00:05:59 -0400

    complete redirect after signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Products from './components/Products';
 import Signup from "./pages/Signup";
@@ -7,7 +7,7 @@ import Login from './pages/Login';
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Products />} />
@@ -15,7 +15,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
